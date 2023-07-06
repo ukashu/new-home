aa32ae4a7ac30e95da017e1d638de768da3aef99
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-07-06 12:31:09 +0200

    fixed Tooltip z-index

--- src/newtab/components/utility/Tooltip.tsx
+++ src/newtab/components/utility/Tooltip.tsx
@@ -2,7 +2,7 @@ export default function Tooltip({ message, children }) {
   return (
   <div className="group relative flex">
       {children}
-      <span className="absolute left-10 scale-0 transition-all rounded backdrop-blur p-2 text-xs text-white group-hover:scale-100">{message}</span>
+      <span className="absolute z-[1000] left-20 scale-0 transition-all rounded backdrop-blur p-2 text-xs text-white group-hover:scale-100">{message}</span>
   </div>
   )
 }
