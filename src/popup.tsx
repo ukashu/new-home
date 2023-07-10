ea28e1af64fbf1f4d47fd1cc33a71baaa0a491c8
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-07-10 13:54:07 +0200

    added basic mode changing in popup

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,47 @@
-import { CountButton } from "~features/count-button"
 
 import "~base.css"
 import "~style.css"
+import React from 'react'
+import { Storage } from "@plasmohq/storage"
 
 function IndexPopup() {
+  const [mode, setMode] = React.useState('')
+
+  const storage = new Storage({
+    area: "local"
+  })
+
+  React.useEffect(() => {
+    readAndSetModeFromStorage()
+  }, [])
+
+  const setModeStorage = async(mode) => {
+    await storage.set('mode', mode)
+    setMode(mode)
+  }
+
+  const readAndSetModeFromStorage = async() => {
+    const savedMode = await storage.get('mode')
+    if (!savedMode) {
+      await storage.set('mode', 'LIFE')
+      setMode('LIFE')
+      return
+    }
+    setMode(savedMode)
+  }
+
   return (
     <div className="flex items-center justify-center h-16 w-40">
-      <CountButton />
+      <button 
+      className={mode === "WORK" ? "flex flex-row items-center px-4 py-2 text-sm rounded-lg transition-all border-none shadow-lg hover:shadow-md active:scale-105 bg-red-400" : "flex flex-row items-center px-4 py-2 text-sm rounded-lg transition-all border-none shadow-lg hover:shadow-md active:scale-105"}
+      onClick={() => setModeStorage('WORK')}>
+        WORK
+      </button>
+      <button 
+      className={mode === "LIFE" ? "flex flex-row items-center px-4 py-2 text-sm rounded-lg transition-all border-none shadow-lg hover:shadow-md active:scale-105 bg-red-400" : "flex flex-row items-center px-4 py-2 text-sm rounded-lg transition-all border-none shadow-lg hover:shadow-md active:scale-105"}
+      onClick={() => setModeStorage('LIFE')}>
+        LIFE
+      </button>
     </div>
   )
 }
