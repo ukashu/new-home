accb50664b7ca89f9259caca2f72835cd7ecb005
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-07-11 16:38:23 +0200

    added PrettyText component

--- src/newtab/components/Websites.tsx
+++ src/newtab/components/Websites.tsx
@@ -2,6 +2,7 @@ import "~base.css"
 import "~style.css"
 import React from "react"
 import { Storage } from "@plasmohq/storage"
+import PrettyText from "./reusable/PrettyText"
 
 export default function Websites() {
   const storage = new Storage({
@@ -28,11 +29,15 @@ export default function Websites() {
         let proportions = Math.floor((Number(sorted[i][1])/total)*100)
         rows.push(
           <div key={sorted[i][0]} className=" text-white flex flex-row justify-between items-center">
-            <p className="w-3/12 flex-grow text-left">{`${sorted[i][0]} `}</p>
+            <div className="w-3/12 flex-grow text-left font-bold">
+              <PrettyText text={sorted[i][0]}/>
+            </div>
             <div className="h-full w-6/12 flex-grow mx-1 flex justify-start items-center">
               <div className={`h-[3px] rounded-md bg-zinc-600`} style={{width: `${proportions}%`}}></div>
             </div>
-            <p className="w-3/12 flex-grow text-right">{`${sorted[i][1]}min / ${proportions}%`}</p>
+            <div className="w-3/12 flex-grow text-right font-bold">
+              <PrettyText text={`${sorted[i][1]}min / ${proportions}%`}/>
+            </div>
           </div>
         )
       }
---
+++ src/newtab/components/reusable/PrettyText.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {
+  text: string,
+  color?: string
+}
+
+export default function PrettyText(props: Props) {
+  return (
+    <p style={{
+      "backgroundImage": `radial-gradient(51.14% 51.14% at 50.23% 62.5%, ${props.color ? props.color : '#9A9A9A'} 0%, #8d8d8d 100%)`,
+      "WebkitBackgroundClip": "text",
+      "WebkitTextFillColor": "transparent",
+      "backgroundClip": "text"
+    }}>{props.text}
+    </p>
+  )
+}
