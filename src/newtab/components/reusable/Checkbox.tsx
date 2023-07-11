5c8ae8421f5babb8a14b63a336bd5023ba32a1b3
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-07-11 17:17:35 +0200

    changed checkbox style

--- src/newtab/components/reusable/Checkbox.tsx
+++ src/newtab/components/reusable/Checkbox.tsx
@@ -1,10 +1,16 @@
+import { AiOutlineCheck } from 'react-icons/ai'
+import { AiOutlineClose } from 'react-icons/ai'
+
 export default function Checkbox(props: { state: boolean, onClick: () => void }) {
 
   return (
     props.state ?
-    <div className='h-full aspect-square rounded-lg bg-red-600' onClick={props.onClick}>
+    <div className='h-full aspect-square' onClick={props.onClick}>
+      <AiOutlineCheck color='red' size="20" style={{position: "absolute"}}/>
     </div>
     :
-    <div className='h-full aspect-square rounded-lg bg-slate-200' onClick={() => props.onClick()}></div>
+    <div className='h-full aspect-square' onClick={props.onClick}>
+      <AiOutlineClose color='#4a4a4a' size="20" style={{position: "absolute"}}/>
+    </div>
   )
 }
