7a311122c2b8ae3275c207693f321da3c2e4846d
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-07-11 17:22:46 +0200

    changed proportions

--- src/newtab/components/Websites.tsx
+++ src/newtab/components/Websites.tsx
@@ -26,17 +26,17 @@ export default function Websites() {
       //generate components
       for (let i in sorted as any) {
         if (sorted[i][0] === "date") {continue}
-        let proportions = Math.floor((Number(sorted[i][1])/total)*100)
+        let percentage = Math.floor((Number(sorted[i][1])/total)*100)
         rows.push(
           <div key={sorted[i][0]} className=" text-white flex flex-row justify-between items-center">
             <div className="w-3/12 flex-grow text-left font-bold">
               <PrettyText text={sorted[i][0]}/>
             </div>
             <div className="h-full w-6/12 flex-grow mx-1 flex justify-start items-center">
-              <div className={`h-[3px] rounded-md bg-zinc-600`} style={{width: `${proportions}%`}}></div>
+              <div className={`h-[3px] rounded-md bg-zinc-600`} style={{width: `${percentage}%`}}></div>
             </div>
             <div className="w-3/12 flex-grow text-right font-bold">
-              <PrettyText text={`${sorted[i][1]}min / ${proportions}%`}/>
+              <PrettyText text={`${sorted[i][1]}min / ${percentage}%`}/>
             </div>
           </div>
         )
--- src/newtab/components/reusable/Shortcut.tsx
+++ src/newtab/components/reusable/Shortcut.tsx
@@ -8,13 +8,13 @@ export default function Shortcut(props: Props) {
   function faviconURL(u) {
     const url = new URL(chrome.runtime.getURL("/_favicon/"));
     url.searchParams.set("pageUrl", u);
-    url.searchParams.set("size", "30");
+    url.searchParams.set("size", "26");
     return url.toString();
   }
 
   return (
     <Tooltip key={props.websiteURL} message={props.websiteURL}>
-      <div onClick={() => window.open(props.websiteURL, '_self')} onMouseDown={e => (e.button === 1) && window.open(props.websiteURL)} className="h-[80px] aspect-square text-slate-200 bg-black flex items-center justify-center rounded-lg cursor-pointer">
+      <div onClick={() => window.open(props.websiteURL, '_self')} onMouseDown={e => (e.button === 1) && window.open(props.websiteURL)} className="h-[70px] aspect-square text-slate-200 bg-black flex items-center justify-center rounded-lg cursor-pointer">
       <img src={faviconURL(props.websiteURL)}/>
       </div>
     </Tooltip>
