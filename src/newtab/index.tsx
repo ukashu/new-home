227993c969bdc913e7b820a17138d6cc553959a8
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-07-14 10:00:01 +0200

    changed background

--- src/newtab/index.tsx
+++ src/newtab/index.tsx
@@ -11,8 +11,6 @@ import Initializer from "./components/reusable/Initializer"
 import { Storage } from "@plasmohq/storage"
 import React from "react"
 
-import newTabBackground from '../resources/newTabBackground.svg'
-
 function IndexNewtab() {
   const [initialized, setInitialized] = React.useState({
     spotifyOn: false,
@@ -52,14 +50,6 @@ function IndexNewtab() {
 
   return (
     <div className="flex flex-row justify-between h-screen p-2 gap-2 font-[Inter]">
-      <img src={newTabBackground} alt="newTabBackground" style={{
-        'position': 'absolute',
-        'zIndex': '-999',
-        'top': '0',
-        'left': '0',
-        'height': '100%',
-        'width': '100%'
-      }}/>
       <section id="left" className="flex flex-col items-center w-1/5 min-w-[300px] h-100% gap-2">
         <Todos/>
         <Quotes quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." author="Lorem Ipsum"/>
