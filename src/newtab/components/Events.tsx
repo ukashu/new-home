9e5f865b8330b047e37b006d46453f6ee59ca30e
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-07-21 10:02:34 +0200

    Events refactoring done

--- src/newtab/components/Events.tsx
+++ src/newtab/components/Events.tsx
@@ -2,9 +2,74 @@ import React from "react"
 import "~base.css"
 import "~style.css"
 
-type Props = {}
+function createGoogleCalendarEndpoint(email: string): string {
+  let offset = new Date().getTimezoneOffset()
+  let timeNow = new Date(Date.now() - offset * 60 * 1000).toISOString()
+  let timeTomorrow: any = new Date()
+  timeTomorrow.setHours(24, 0, 0, 0)
+  timeTomorrow.setHours(timeTomorrow.getHours() + 2)
+  timeTomorrow = timeTomorrow.toISOString()
+  let params = new URLSearchParams({
+    orderBy: "startTime",
+    singleEvents: "true",
+    timeMin: `${timeNow.split(".")[0]}${getFormattedTimezoneOffset()}`,
+    timeMax: `${timeTomorrow.split(".")[0]}${getFormattedTimezoneOffset()}`,
+    key: process.env.PLASMO_PUBLIC_GOOGLE_API_KEY
+  })
+  let url = `https://www.googleapis.com/calendar/v3/calendars/${email}/events?${params}`
+  return url
+}
 
-function Events(props: Props) {
+async function getToken() {
+  const token: any = await chrome.identity.getAuthToken({
+    interactive: true
+  })
+  return token.token
+}
+
+async function getEmail() {
+  const user: any = await chrome.identity.getProfileUserInfo({
+    accountStatus: "ANY" as any
+  })
+  return user.email
+}
+
+async function getEvents(token: string) {
+  const email: any = await getEmail()
+  const fetch_url = createGoogleCalendarEndpoint(email)
+  const fetch_options = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json"
+    }
+  }
+  let res = await fetch(fetch_url, fetch_options)
+  res = await res.json()
+  return res
+}
+
+async function getColors(token: string) {
+  const fetch_options = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json"
+    }
+  }
+  let res = await fetch(
+    `https://www.googleapis.com/calendar/v3/colors?key=${process.env.PLASMO_PUBLIC_GOOGLE_API_KEY}`,
+    fetch_options
+  )
+  res = await res.json()
+  return res
+}
+
+function getFormattedTimezoneOffset() {
+  let offset: any = new Date(Date.now()).toString().split("GMT")[1].split(" ")[0]
+  offset = offset.split("")
+  return `${offset[0]}${offset[1]}${offset[2]}:${offset[3]}${offset[4]}`
+}
+
+function Events() {
   const [currentEvent, setCurrentEvent] = React.useState({
     name: " ",
     color: "#9A9A9A"
@@ -15,79 +80,30 @@ function Events(props: Props) {
   })
 
   React.useEffect(() => {
-    getAndSetEvents()
+    const initialize = async () => {
+      const token = await getToken()
+      const colors = await getColors(token)
+      const events = await getEvents(token)
+      setEvents(events, colors)
+    }
+    initialize().catch(console.error)
   }, [])
 
-  function createGoogleCalendarEndpoint(email: string): string {
-    let offset = new Date().getTimezoneOffset()
-    let timeNow = new Date(Date.now() - offset * 60 * 1000).toISOString()
-    let timeTomorrow: any = new Date()
-    timeTomorrow.setHours(24, 0, 0, 0)
-    timeTomorrow.setHours(timeTomorrow.getHours() + 2)
-    timeTomorrow = timeTomorrow.toISOString()
-    let params = new URLSearchParams({
-      orderBy: "startTime",
-      singleEvents: "true",
-      timeMin: `${timeNow.split(".")[0]}${getFormattedTimezoneOffset()}`,
-      timeMax: `${timeTomorrow.split(".")[0]}${getFormattedTimezoneOffset()}`,
-      key: process.env.PLASMO_PUBLIC_GOOGLE_API_KEY
-    })
-    let url = `https://www.googleapis.com/calendar/v3/calendars/${email}/events?${params}`
-    return url
-  }
-
-  async function getAndSetEvents() {
-    try {
-      const user: any = await chrome.identity.getProfileUserInfo({
-        accountStatus: "ANY" as any
+  function setEvents(events, colors) {
+    let curr = events.items[0].summary
+    if (curr) {
+      setCurrentEvent({
+        name: curr,
+        color: events.items[0].colorId ? colors.event[events.items[0].colorId].background : "#3083FF"
       })
-      const token: any = await chrome.identity.getAuthToken({
-        interactive: true
-      })
-      const fetch_url = createGoogleCalendarEndpoint(user.email)
-      const fetch_options = {
-        headers: {
-          Authorization: `Bearer ${token.token}`,
-          Accept: "application/json"
-        }
-      }
-      const colorApiResponse = await fetch(
-        `https://www.googleapis.com/calendar/v3/colors?key=${process.env.PLASMO_PUBLIC_GOOGLE_API_KEY}`,
-        fetch_options
-      )
-      const colors = await colorApiResponse.json()
-      const res = await fetch(fetch_url, fetch_options)
-      const events = await res.json()
-      let curr = events.items[0].summary
-      if (curr) {
-        setCurrentEvent({
-          name: curr,
-          color: events.items[0].colorId
-            ? colors.event[events.items[0].colorId].background
-            : "#3083FF"
-        })
-      }
-      let next = events.items[1].summary
-      if (next) {
-        setNextEvent({
-          name: next,
-          color: events.items[1].colorId
-            ? colors.event[events.items[1].colorId].background
-            : "#3083FF"
-        })
-      }
-    } catch (err) {
-      console.error({ err })
     }
-  }
-
-  function getFormattedTimezoneOffset() {
-    let offset: any = new Date(Date.now())
-      .toString()
-      .split("GMT")[1]
-      .split(" ")[0]
-    offset = offset.split("")
-    return `${offset[0]}${offset[1]}${offset[2]}:${offset[3]}${offset[4]}`
+    let next = events.items[1].summary
+    if (next) {
+      setNextEvent({
+        name: next,
+        color: events.items[1].colorId ? colors.event[events.items[1].colorId].background : "#3083FF"
+      })
+    }
   }
 
   return (
